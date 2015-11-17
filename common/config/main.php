2d6bf88f2bf4ba72b2869c30dc3434415e09d35b
gabyckaz <gaby_dva_@hotmail.com>
2015-11-17 03:17:12 -0600

    con rbac

--- common/config/main.php
+++ common/config/main.php
@@ -1,9 +1,16 @@
-<?php
+
+ <?php
 return [
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
+        //para el rbac
+         'authManager' => [
+            'class' => 'yii\rbac\DbManager',
+        ],
     ],
+
+    
 ];
